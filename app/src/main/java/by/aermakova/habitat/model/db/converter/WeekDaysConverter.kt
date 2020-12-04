7af0bc6397dd4f3a9bb35cfef24028fba37e8058
Anna <hannayermakova@mail.ru>
2020-12-04 09:27:15 +0300

    Remove idea derectory

--- app/src/main/java/by/aermakova/habitat/model/db/converter/WeekDaysConverter.kt
+++ app/src/main/java/by/aermakova/habitat/model/db/converter/WeekDaysConverter.kt
@@ -4,7 +4,7 @@ import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
-object dxxWeekDaysConverter {
+object WeekDaysConverter {
     private val gson = Gson()
 
     @kotlin.jvm.JvmStatic
