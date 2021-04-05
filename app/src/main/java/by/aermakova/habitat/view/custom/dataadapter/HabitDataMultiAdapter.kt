40ac4cfd2cf7a1bc3a47170bd8bfc515983618e8
Anna <hannayermakova@mail.ru>
2021-04-05 17:16:14 +0300

    update habits list

--- app/src/main/java/by/aermakova/habitat/view/custom/dataadapter/HabitDataMultiAdapter.kt
+++ app/src/main/java/by/aermakova/habitat/view/custom/dataadapter/HabitDataMultiAdapter.kt
@@ -1,49 +1,62 @@
 package by.aermakova.habitat.view.custom.dataadapter
 
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import by.aermakova.habitat.model.db.entity.Habit
-import by.aermakova.habitat.util.FunctionNoArgs
 import by.aermakova.habitat.view.custom.HabitCardAdditionalView
 import by.aermakova.habitat.view.custom.HabitCardView
+import by.aermakova.habitat.view.navigation.MainFlowNavigation
 
-class HabitDataMultiAdapter(private val clickListener: FunctionNoArgs) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private var habitList: List<Habit?>? = null
+private const val ADDITIONAL_TYPE = 0
+private const val REGULAR_TYPE = 1
 
-    override fun getItemViewType(position: Int): Int {
-        return if (habitList == null || position == habitList!!.size) 0 else 1
-    }
+class HabitDataMultiAdapter(private val routing: MainFlowNavigation) :
+    ListAdapter<Habit, RecyclerView.ViewHolder>(DiffCallback) {
 
-    override fun getItemCount(): Int {
-        return if (habitList != null) habitList!!.size + 1 else 1
-    }
+    companion object DiffCallback : DiffUtil.ItemCallback<Habit>() {
 
-    fun setHabitList(habitList: List<Habit?>?) {
-        this.habitList = habitList
-        notifyDataSetChanged()
-    }
+        override fun areItemsTheSame(oldItem: Habit, newItem: Habit): Boolean {
+            return oldItem.id == newItem.id
+        }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return if (viewType == 0) {
-            HabitViewAdditionHolder(HabitCardAdditionalView(parent.context))
-        } else HabitViewHolder(HabitCardView(parent.context))
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder.itemViewType == 0) {
-            (holder as HabitViewAdditionHolder).view.setTextVisible(!(habitList != null && habitList!!.isNotEmpty()))
-        } else (holder as HabitViewHolder).view.setHabitItem(habitList!![position])
-    }
-
-    internal class HabitViewHolder(val view: HabitCardView) : RecyclerView.ViewHolder(view)
-
-    internal inner class HabitViewAdditionHolder(val view: HabitCardAdditionalView) :
-        RecyclerView.ViewHolder(view) {
-
-        init {
-            view.setOnClickListener { clickListener.invoke() }
+        override fun areContentsTheSame(oldItem: Habit, newItem: Habit): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
         }
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return if (itemCount == 1 || position == itemCount - 1) ADDITIONAL_TYPE else REGULAR_TYPE
+    }
+
+    override fun getItemCount(): Int {
+        return super.getItemCount() + 1
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            ADDITIONAL_TYPE -> HabitViewAdditionHolder(HabitCardAdditionalView(parent.context))
+            else -> HabitViewHolder(HabitCardView(parent.context))
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder.itemViewType) {
+            ADDITIONAL_TYPE -> {
+                (holder as HabitViewAdditionHolder).view.setTextVisible(itemCount <= 1)
+                holder.view.setOnClickListener { routing.navigateToAddNewElementFragment() }
+            }
+            else -> {
+                (holder as HabitViewHolder).view.setHabitItem(getItem(position))
+                holder.view.setOnClickListener {
+                    routing.navigateToShowDetailsFragment(getItem(position).id)
+                }
+            }
+        }
+    }
+
+    class HabitViewHolder(val view: HabitCardView) : RecyclerView.ViewHolder(view)
+
+    class HabitViewAdditionHolder(val view: HabitCardAdditionalView) : RecyclerView.ViewHolder(view)
 }
