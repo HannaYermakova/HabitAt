be0074657bfe398dca926136c6c79638645f5f00
Anna <hannayermakova@mail.ru>
2021-04-09 16:54:58 +0300

    category pill

--- app/src/main/java/by/aermakova/habitat/view/custom/dataadapter/CategoryPillsAdapter.kt
+++ app/src/main/java/by/aermakova/habitat/view/custom/dataadapter/CategoryPillsAdapter.kt
@@ -8,9 +8,9 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import by.aermakova.habitat.R
+import by.aermakova.habitat.databinding.CustomPillCreateFirstHabitDataBinding
 import by.aermakova.habitat.databinding.CustomToggleCategoryPillBinding
 import by.aermakova.habitat.model.model.CategoryWrapper
-import by.aermakova.habitat.view.custom.CategoryPillButtonView
 
 private const val ADDITIONAL_TYPE = 0
 private const val REGULAR_TYPE = 1
@@ -44,10 +44,19 @@ class CategoryPillsAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
         return when (viewType) {
-            ADDITIONAL_TYPE -> CategoryPillCreateFirstViewHolder(CategoryPillButtonView(parent.context))
+            ADDITIONAL_TYPE -> {
+                val binding: CustomPillCreateFirstHabitDataBinding = DataBindingUtil.inflate(
+                    inflater,
+                    R.layout.custom_pill_create_first_habit_data,
+                    parent,
+                    false
+                )
+                CategoryPillCreateFirstViewHolder(binding)
+            }
             else -> {
-                val inflater = LayoutInflater.from(parent.context)
+
                 val binding: CustomToggleCategoryPillBinding = DataBindingUtil.inflate(
                     inflater,
                     R.layout.custom_toggle_category_pill,
@@ -61,13 +70,15 @@ class CategoryPillsAdapter(
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder.itemViewType) {
-            ADDITIONAL_TYPE -> (holder as CategoryPillCreateFirstViewHolder).view.setOnClickListener { openCreateCategory.invoke() }
+            ADDITIONAL_TYPE -> {
+                (holder as CategoryPillCreateFirstViewHolder).binding.root.setOnClickListener { openCreateCategory.invoke() }
+            }
             else -> {
                 val wrapper = getItem(position)
                 val binding = (holder as CategoryPillViewHolder).binding
                 binding.category = wrapper
                 (binding.root as ToggleButton).setOnCheckedChangeListener { _, isChecked ->
-                    if(isChecked) update.invoke(wrapper)
+                    if (isChecked) update.invoke(wrapper)
                     notifyDataSetChanged()
                 }
             }
@@ -77,6 +88,6 @@ class CategoryPillsAdapter(
     class CategoryPillViewHolder(val binding: CustomToggleCategoryPillBinding) :
         RecyclerView.ViewHolder(binding.root)
 
-    class CategoryPillCreateFirstViewHolder(val view: CategoryPillButtonView) :
-        RecyclerView.ViewHolder(view)
+    class CategoryPillCreateFirstViewHolder(val binding: CustomPillCreateFirstHabitDataBinding) :
+        RecyclerView.ViewHolder(binding.root)
 }
